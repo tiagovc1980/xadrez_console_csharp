50f65d584f6d878cbfc46e4e4c4b47672af9b7c2
Tiago VC <vc.tiago@gmail.com>
2023-02-06 12:14:44 -0300

    Criando a classe posição Xadrez

--- gXadrez_Console/Program.cs
+++ gXadrez_Console/Program.cs
@@ -8,13 +8,9 @@ internal class Program
     {
         try
         {
-            TabuleiroDoJogo tab = new TabuleiroDoJogo(8, 8);
-
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-
-            Tela.ImprimirTabuleiro(tab);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.PosicaoNaMatriz());
         }
         catch (TabuleiroException e)
         {
---
+++ gXadrez_Console/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tabuleiro;
+
+namespace gXadrez_Console.Xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char LetraNaColuna { get; set; }
+        public int NumeroNaLinha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            LetraNaColuna= coluna;
+            NumeroNaLinha= linha;
+        }
+
+        public Posicao PosicaoNaMatriz ()
+        {
+            return new Posicao(8 - NumeroNaLinha, LetraNaColuna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + LetraNaColuna + NumeroNaLinha;
+        }
+    }
+}
